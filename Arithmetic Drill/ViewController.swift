fe1331fbb4af82fcc57f21d0ae644afa7423b187
tkumata <tkumata@gmail.com>
2016-11-07 12:37:19 +0900

    1st commit.

--- Arithmetic Drill/ViewController.swift
+++ Arithmetic Drill/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func backToHome (segue: UIStoryboardSegue) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
