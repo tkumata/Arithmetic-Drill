823a9304b70181a0c3899e5936130aa535016189
tkumata <tkumata@gmail.com>
2016-11-18 05:47:23 +0900

    change some var names.

--- Arithmetic Drill/ArithmeticViewController.swift
+++ Arithmetic Drill/ArithmeticViewController.swift
@@ -15,7 +15,7 @@ class ArithmeticViewController: UIViewController, UITextFieldDelegate, KeyboardD
     // Read User Default
     let userData = UserDefaults.standard
     
-    var answer: Int = 0, userAnswer: Int = 0
+    var maskedAnswer: Int = 0, userAnswer: Int = 0
     var timer: Timer!
     var timerCounter: Int = 11
     
@@ -26,10 +26,10 @@ class ArithmeticViewController: UIViewController, UITextFieldDelegate, KeyboardD
     var accuracyRate: Double = 0.0
     
     // Initialize variable from User Default.
-    var levelOnSettings: Int = 5
-    var burstModeOnSettings: Bool = false
-    var disable10OnSettings: Bool = false
-    var kukuModeOnSettings: Bool = false
+    var settingsLevel: Int = 5
+    var settingsBurstMode: Bool = false
+    var settingsDisable10: Bool = false
+    var settingsKukuMode: Bool = false
 
     // Image of result of checking answer.
     var imageView: UIImageView!
@@ -62,16 +62,16 @@ class ArithmeticViewController: UIViewController, UITextFieldDelegate, KeyboardD
         var questionString: String = ""
         var leftTerm1: Int = 0
         var leftTerm2: Int = 0
-        var kigouNum: Int = 3
+        var mathSymbolNum: Int = 3
+        var mathSymbol: String = ""
         var rightTerm: Int = 0
-        var kigou: String = ""
         
-        if kukuModeOnSettings {
-            levelOnSettings = 1
+        if settingsKukuMode {
+            settingsLevel = 1
         }
         
         // Make question parts.
-        switch levelOnSettings {
+        switch settingsLevel {
         case 1:
             leftTerm1 = Int(arc4random_uniform(9)+1) // 0~8 + 1 = 1~9
             leftTerm2 = Int(arc4random_uniform(9)+1)
@@ -107,54 +107,54 @@ class ArithmeticViewController: UIViewController, UITextFieldDelegate, KeyboardD
             leftTerm2 = Int(arc4random_uniform(16)+5)
         }
 
-        if levelOnSettings > 0 && levelOnSettings < 3 {
-            kigouNum = Int(arc4random_uniform(2))
-        } else if kukuModeOnSettings {
-            kigouNum = 2
+        if settingsLevel > 0 && settingsLevel < 3 {
+            mathSymbolNum = Int(arc4random_uniform(2))
+        } else if settingsKukuMode {
+            mathSymbolNum = 2
         } else {
-            kigouNum = Int(arc4random_uniform(3))
+            mathSymbolNum = Int(arc4random_uniform(3))
         }
         
         // 記号の生成
-        switch kigouNum {
+        switch mathSymbolNum {
         case 0:
             rightTerm = leftTerm1 + leftTerm2
-            kigou = "+"
+            mathSymbol = "+"
         case 1:
             rightTerm = leftTerm1 - leftTerm2
-            kigou = "-"
+            mathSymbol = "-"
         case 2:
             rightTerm = leftTerm1 * leftTerm2
-            kigou = "*"
+            mathSymbol = "*"
         default:
             rightTerm = leftTerm1 + leftTerm2
-            kigou = "+"
+            mathSymbol = "+"
         }
         
         // Decide masking part.
-        let maskNum = Int(arc4random_uniform(3))
+        let maskNumber = Int(arc4random_uniform(3)) // 0, 1, 2
         
         // Display question.
-        switch maskNum {
+        switch maskNumber {
         case 0:
-            answer = leftTerm1
-            questionString = "X " + kigou + " " + String(leftTerm2) + " = " + String(rightTerm)
+            maskedAnswer = leftTerm1
+            questionString = "X " + mathSymbol + " " + String(leftTerm2) + " = " + String(rightTerm)
         case 1:
-            answer = leftTerm2
-            questionString = String(leftTerm1) + " " + kigou + " X" + " = " + String(rightTerm)
+            maskedAnswer = leftTerm2
+            questionString = String(leftTerm1) + " " + mathSymbol + " X" + " = " + String(rightTerm)
         case 2:
-            answer = rightTerm
-            questionString = String(leftTerm1) + " " + kigou + " " + String(leftTerm2) + " = X"
+            maskedAnswer = rightTerm
+            questionString = String(leftTerm1) + " " + mathSymbol + " " + String(leftTerm2) + " = X"
         default:
-            answer = leftTerm2
-            questionString = "X " + kigou + " " + String(leftTerm2) + " = " + String(rightTerm)
+            maskedAnswer = leftTerm2
+            questionString = "X " + mathSymbol + " " + String(leftTerm2) + " = " + String(rightTerm)
         }
         
-        
+        // Display question.
         self.questionLabel.text = questionString
         
-        // Make timer.
-        if disable10OnSettings == false {
+        // Make timer or not.
+        if settingsDisable10 == false {
             timerCounter = 11
             
             // Timer
@@ -179,15 +179,15 @@ class ArithmeticViewController: UIViewController, UITextFieldDelegate, KeyboardD
         // Attributes.
         questionLabel.text = ""
         messageLabel.text = ""
-        messageLabel.layer.cornerRadius = 5.0
+        messageLabel.layer.cornerRadius = 10.0
         messageLabel.layer.borderWidth = 1.0
         messageLabel.layer.borderColor = UIColor(red: 200/255, green: 200/255, blue: 200/255, alpha: 1.0).cgColor
         
         // Read settings in User Default.
-        levelOnSettings = userData.integer(forKey: "LEVEL")
-        burstModeOnSettings = userData.bool(forKey: "BURSTMODE")
-        disable10OnSettings = userData.bool(forKey: "DISABLE10")
-        kukuModeOnSettings = userData.bool(forKey: "99MODE")
+        settingsLevel = userData.integer(forKey: "LEVEL")
+        settingsBurstMode = userData.bool(forKey: "BURSTMODE")
+        settingsDisable10 = userData.bool(forKey: "DISABLE10")
+        settingsKukuMode = userData.bool(forKey: "99MODE")
         hiscore = userData.integer(forKey: "HISCORE")
         hiAccuracyRate = userData.double(forKey: "HIRATE")
         
@@ -233,11 +233,11 @@ class ArithmeticViewController: UIViewController, UITextFieldDelegate, KeyboardD
         super.viewWillDisappear(animated)
         stopTimer()
         
-        burstModeOnSettings = false
+        settingsBurstMode = false
         userAnswerTxtField.isEnabled = false
         userAnswerTxtField.resignFirstResponder()
         
-        if burstModeOnSettings && questionNumber > 1 {
+        if settingsBurstMode && questionNumber > 1 {
             questionNumber -= 1
             userData.set(questionNumber, forKey: "QNUM")
         }
@@ -246,7 +246,7 @@ class ArithmeticViewController: UIViewController, UITextFieldDelegate, KeyboardD
 
     // MARK: - 入力終了(キーボードが退場) = 答え合わせ
     func textFieldDidEndEditing(_ textField: UITextField, reason: UITextFieldDidEndEditingReason) {
-        var messageTmp: String = ""
+        var tmpMessage: String = ""
         
         // Stop timer.
         stopTimer()
@@ -264,7 +264,7 @@ class ArithmeticViewController: UIViewController, UITextFieldDelegate, KeyboardD
         userAnswer = (userAnswerText as NSString).integerValue
         
         // MARK: Check answer.
-        if (self.answer == userAnswer) {
+        if (self.maskedAnswer == userAnswer) {
             // Correct
             questionCorrect += 1
 
@@ -273,17 +273,17 @@ class ArithmeticViewController: UIViewController, UITextFieldDelegate, KeyboardD
             
             if timerCounter > 0 {
                 score += timerCounter
-                messageTmp = "Correct"
+                tmpMessage = "Correct"
             }
             
             // Bonus point
             if timerCounter > 7 {
                 score += 10
-                messageTmp += " and bounus point"
+                tmpMessage += " and bounus point"
             }
             
             // Display image
-            if burstModeOnSettings == false {
+            if settingsBurstMode == false {
                 checkResultImg(result: true)
             }
         } else {
@@ -291,19 +291,19 @@ class ArithmeticViewController: UIViewController, UITextFieldDelegate, KeyboardD
             questionIncorrect += 1
             
             if timerCounter == 0 {
-                messageTmp = "Time up"
+                tmpMessage = "Time up"
             } else {
-                messageTmp = "Incorrect\n" + String(answer)
+                tmpMessage = "Incorrect\n" + String(maskedAnswer)
             }
             
             // Display image
-            if burstModeOnSettings == false && userAnswer != 0 {
+            if settingsBurstMode == false && userAnswer != 0 {
                 checkResultImg(result: false)
             }
         }
         
         // Display message.
-        messageLabel.text = messageTmp
+        messageLabel.text = tmpMessage
         
         // Overwrite score with accuracy rate
         if questionCorrect > 0 && questionNumber > 0 {
@@ -326,7 +326,7 @@ class ArithmeticViewController: UIViewController, UITextFieldDelegate, KeyboardD
         }
         
         // Burst mode
-        if burstModeOnSettings {
+        if settingsBurstMode {
             nextQuestionButton(self)
         }
     }
